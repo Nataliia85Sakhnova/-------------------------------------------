434a833e5eee31ee0659faacb124d0cdb0f185a1
Razhatawka <nataliia85sakhnova@gmail.com>
2022-07-25 11:51:27 +0300

    c

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const validPetsname = (nameField) => {
-    var nameFormat = /^[a-zA-Zа-яёА-ЯЁ]+$/u;
+const validName = (nameField) => {
+    const nameFormat = /^[a-zA-Zа-яёА-ЯЁ]+$/u;
     if (nameField.value.match(nameFormat)) {
+        console.log(nameField.value)
         return true;
     } else {
         alert("Еще раз проверьте!");
+        console.log("Валидация завалена! Иди переделывай!")
         return false;
     }
 }
+
+// сначала eventlistener, а в нем вызов
+// eventlistener  - событие клика по кнопке запускает действие функции,
+// которая проверяет валидацию полей формы.
+
+// element.addEventListener("click", validName)
+
